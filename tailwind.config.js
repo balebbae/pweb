15cd7991298fe0afa64833c6afe6a7f4336b7d44
Caleb Bae <baecal000@gmail.com>
2024-11-24 11:11:44 -0600

    starter

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/hooks/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/data.json',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E8E7E3',
+        secondary: '#565449',
+
+        text: '#11120D',
+        background: '#11120D',
+      },
+
+      fontFamily: {
+        heading: ['var(--font-heading)', 'serif'],
+        body: ['var(--font-body)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
